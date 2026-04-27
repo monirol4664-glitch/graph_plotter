74de56442ee38c71176c4a626ecb110c6d24c825
monirol4664-glitch <monirol4664@gmail.com>
2026-04-27 10:26:45 +0600

    Implement download listener in MainActivity
    Added download listener to handle PDF/CSV downloads from WebView.

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,10 @@
 package com.attendance.digital;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.DownloadListener;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -12,8 +15,20 @@ public class MainActivity extends Activity {
         WebView webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
         webView.setWebViewClient(new WebViewClient());
+        
+        // Enable PDF/CSV downloads from WebView
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+        
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
     }
-}
+            }
